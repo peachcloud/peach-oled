b759254afbce11ad2aea18508accad3b5c253e65
mycognosist <gnomad@cryptolab.net>
2019-03-21 15:31:28 +0000

    Add validation for write parameters and update README

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ OLED microservice module for PeachCloud. Write to a 128x64 OLED display with SDD
 
 ![Close-up, black-and-white photo of an Adafruit 128x64 1.3" OLED Bonnet. The circuit board features a 5-way joystick on the left side, two push-buttons on the right side (labelled #5 and #6), and a central OLED display. The display shows text reading: "PeachCloud" on the first line and "IP: 192.168.0.8" on the third line. A circle is displayed beneath the two lines of text and is horizontally-centered".](docs/images/peachcloud_oled.jpg)
 
+### JSON-RPC API
+
+| Method | Parameters | Description |
+| --- | --- | --- |
+| `write` | `x_coord`, `y_coord`, `string` | Write message to display at given co-ordinates |
+| `clear` | | Clear the display |
+
 ### Setup
 
 Clone this repo:
@@ -19,12 +26,52 @@ Run the binary:
 
 `./target/debug/peach-oled`
 
-### JSON-RPC
+### Example Usage
 
-| Method | Parameters | Description |
-| --- | --- | --- |
-| `write` | `x_coord`, `y_coord`, `string` | Write message to display at given co-ordinates |
-| `clear` | | Clear the display |
+**Write Text to the OLED Display**
+
+With microservice running, open a second terminal window and use `curl` to call server methods:
+
+`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "write", "params" : {"x_coord": 0, "y_coord": 0, "string": "Welcome to PeachCloud!" }, "id":1 }' 127.0.0.1:3030`
+
+Server responds with:
+
+`{"jsonrpc":"2.0","result":success","id":1}`
+
+OLED display shows:
+
+`Welcome to PeachCloud!`
+
+Write to the second line of the display:
+
+`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "write", "params" : {"x_coord": 0, "y_coord": 8, "string": "Born in cypherspace" }, "id":1 }' 127.0.0.1:3030`
+
+OLED display shows:
+
+`Welcome to PeachCloud!`  
+`Born in cypherspace`
+
+Validation checks are performed for all three parameters: `x_coord`, `y_coord` and `string`. An appropriate error is returned if the validation checks are not satisfied:
+
+`{"jsonrpc":"2.0","error":{"code":1,"message":"validation error","data":"x_coord not in range 0-128"},"id":1}`
+
+`{"jsonrpc":"2.0","error":{"code":1,"message":"validation error","data":"y_coord not in range 0-57"},"id":1}`
+
+`{"jsonrpc":"2.0","error":{"code":1,"message":"validation error","data":"string length > 21 characters"},"id":1}`
+
+An error is returned if one or all of the expected parameters are not supplied:
+
+`{"jsonrpc":"2.0","error":{"code":-32602,"message":"invalid params","data":"Invalid params: missing field `string`."},"id":1}`
+
+-----
+
+**Clear the Display**
+
+`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "clear", "id":1 }' 127.0.0.1:3030`
+
+Server responds with:
+
+`{"jsonrpc":"2,0","result":"success","id":1}`
 
 ### Licensing
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate embedded_graphics;
 extern crate ssd1306;
 extern crate machine_ip;
 extern crate failure;
+extern crate validator;
+#[macro_use]
+extern crate validator_derive;
 
 use failure::Fail;
 use hal::I2cdev;
@@ -16,17 +19,24 @@ use jsonrpc_http_server::jsonrpc_core::*;
 use jsonrpc_http_server::*;
 use serde::Deserialize;
 use std::sync::{Arc, Mutex};
+use validator::{Validate, ValidationErrors};
 
 //define the Msg struct for receiving display write commands
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Validate, Deserialize)]
 struct Msg {
+    #[validate(range(min = "0", max = "128", message = "x_coord not in range 0-128"))]
     x_coord: i32,
+    #[validate(range(min = "0", max = "57", message = "y_coord not in range 0-57"))]
     y_coord: i32,
+    #[validate(length(max = "21", message = "string length > 21 characters"))]
     string: String,
 }
 
 #[derive(Debug, Fail)]
 pub enum WriteError {
+    #[fail(display = "validation error")]
+    Invalid { e: ValidationErrors },
+
     #[fail(display = "missing expected parameters")]
     MissingParams {e: Error},
 }
@@ -34,6 +44,32 @@ pub enum WriteError {
 impl From<WriteError> for Error {
     fn from(err: WriteError) -> Self {
         match &err {
+            WriteError::Invalid {e} => {
+                let err_clone = e.clone();
+                // extract error from ValidationErrors
+                let field_errs = err_clone.field_errors();
+                let checks = vec!["x_coord", "y_coord", "string"];
+                // check source of validation err
+                for &error in &checks {
+                    let validation_err = field_errs.get(&error);
+                    if validation_err.is_some() {
+                        let validation_err = validation_err.unwrap();
+                        let err_msg = &validation_err[0].message;
+                        let em = err_msg.clone();
+                        let em = em.expect("failed to unwrap error msg");
+                        return Error {
+                            code: ErrorCode::ServerError(1),
+                            message: "validation error".into(),
+                            data: Some(format!("{}", em).into()),
+                        };
+                    }
+                }
+                Error {
+                    code: ErrorCode::ServerError(1),
+                    message: "validation error".into(),
+                    data: Some(format!("{:?}", e).into()),
+                }
+            }
             WriteError::MissingParams {e} => Error {
                 code: ErrorCode::ServerError(-32602),
                 message: "invalid params".into(),
@@ -67,14 +103,19 @@ fn main() {
             // if result contains parameters, unwrap
             Ok(_) => {
                 let m: Msg = m.unwrap();
-                let mut oled = oled_clone.lock().unwrap();
-                oled.draw(
-                    Font6x8::render_str(&format!("{}", &m.string))
-                        .translate(Coord::new(m.x_coord, m.y_coord))
-                        .into_iter(),
-                );
-                oled.flush().unwrap();
-                Ok(Value::String("success".into()))
+                match m.validate() {
+                    Ok(_) => {
+                        let mut oled = oled_clone.lock().unwrap();
+                        oled.draw(
+                            Font6x8::render_str(&format!("{}", &m.string))
+                                .translate(Coord::new(m.x_coord, m.y_coord))
+                                .into_iter(),
+                        );
+                        oled.flush().unwrap();
+                        Ok(Value::String("success".into()))
+                    }
+                    Err(e) => Err(Error::from(WriteError::Invalid {e})),
+                }
             }
             Err(e) => Err(Error::from(WriteError::MissingParams {e})),
         }
